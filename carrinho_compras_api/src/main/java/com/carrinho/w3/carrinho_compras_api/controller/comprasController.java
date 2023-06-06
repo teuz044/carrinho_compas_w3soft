1f199877204782843aa24736441cf9fec1f2775f
Matheus Gabriel <103612213+teuz044@users.noreply.github.com>
2023-06-06 18:18:48 -0300

    Comunicação estável

--- carrinho_compras_api/src/main/java/com/carrinho/w3/carrinho_compras_api/controller/comprasController.java
+++ carrinho_compras_api/src/main/java/com/carrinho/w3/carrinho_compras_api/controller/comprasController.java
@@ -1,32 +1,31 @@
 package com.carrinho.w3.carrinho_compras_api.controller;
 
 import com.carrinho.w3.carrinho_compras_api.model.comprasModel;
+import com.carrinho.w3.carrinho_compras_api.services.ComprasService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin("*")
 public class comprasController {
-    @PostMapping("/adicionaritem")
-    public ResponseEntity<String> adicionarItem(@RequestBody comprasModel item) {
-        // Lógica para adicionar o item ao banco de dados
-        // Aqui você pode usar um serviço ou repositório para lidar com a persistência
 
-        // Exemplo de resposta simples
+    private final ComprasService comprasService;
+
+    public comprasController(ComprasService comprasService) {
+        this.comprasService = comprasService;
+    }
+    @PostMapping("/adicionaritem")
+    public ResponseEntity<String> adicionarItem(@RequestBody comprasModel comprasModel) {
+        comprasService.adicionarItem(comprasModel);
         String mensagem = "Item adicionado com sucesso";
         return new ResponseEntity<>(mensagem, HttpStatus.OK);
     }
 
     @PostMapping("/removeritem")
-    public ResponseEntity<String> removerItem(@RequestBody comprasModel item) {
-        // Lógica para remover o item do banco de dados
-        // Aqui você pode usar um serviço ou repositório para lidar com a remoção
-
-        // Exemplo de resposta simples
+    public ResponseEntity<String> removerItem(@RequestBody comprasModel comprasModel) {
+        comprasService.removerItem(comprasModel);
         String mensagem = "Item removido com sucesso";
         return new ResponseEntity<>(mensagem, HttpStatus.OK);
     }
